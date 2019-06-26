c5e21f7857dba0b93a8a2e22cfadda9666f28c47
pilz <leandro.pizz@gmail.com>
2019-06-26 12:57:57 -0300

    CAP.375 Cadastro de usuario 03

--- src/components/admin/UserAdmin.vue
+++ src/components/admin/UserAdmin.vue
@@ -9,6 +9,7 @@
               id="user-name"
               type="text"
               v-model="user.name"
+              :readonly="mode === 'remove'"
               required
               placeholder="Informe o Nome do usuário..."
             ></b-form-input>
@@ -20,14 +21,20 @@
               id="user-email"
               type="text"
               v-model="user.email"
+              :readonly="mode === 'remove'"
               required
               placeholder="Informe o E-mail do usuário..."
             ></b-form-input>
           </b-form-group>
         </b-col>
       </b-row>
-      <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3">Administrador?</b-form-checkbox>
-      <b-row>
+      <b-form-checkbox
+        id="user-admin"
+        v-model="user.admin"
+        class="mt-3 mb-3"
+        v-show="mode === 'save'"
+      >Administrador?</b-form-checkbox>
+      <b-row v-show="mode === 'save'">
         <b-col md="6" sm="12">
           <b-form-group label="Senha:" label-for="user-password">
             <b-form-input
@@ -51,12 +58,25 @@
           </b-form-group>
         </b-col>
       </b-row>
-      <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
-      <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
-      <b-button class="ml-2" @click="reset">Cancelar</b-button>
+      <b-row>
+        <b-col xs="12">
+          <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
+          <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
+          <b-button class="ml-2" @click="reset">Cancelar</b-button>
+        </b-col>
+      </b-row>
     </b-form>
     <hr>
-    <b-table hover striped :items="users" :fields="fields"></b-table>
+    <b-table hover striped :items="users" :fields="fields">
+      <template slot="actions" slot-scope="data">
+        <b-button variant="warning" @click="getUser(data.item)" class="mr-2">
+          <i class="fa fa-pencil"></i>
+        </b-button>
+        <b-button variant="danger" @click="getUser(data.item, 'remove')" class="mr-2">
+          <i class="fa fa-trash"></i>
+        </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -116,6 +136,10 @@ export default {
           this.reset();
         })
         .catch(showError);
+    },
+    getUser(user, mode = "save") {
+      this.mode = mode;
+      this.user = { ...user };
     }
   },
   mounted() {
