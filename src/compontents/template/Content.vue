31e436476b019ac7ea2bdd9423bdc364c44c4d5a
pilz <leandro.pizz@gmail.com>
2019-06-25 21:42:51 -0300

    first commit

---
+++ src/compontents/template/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="conten"></div>
+</template>
+
+<script>
+export default {
+  name: "Content"
+};
+</script>
+
+<style>
+.content {
+  grid-area: content;
+  background-color: #efefef;
+  padding: 20px;    
+}
+</style>
