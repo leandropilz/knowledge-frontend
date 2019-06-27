d2c173552c87d6b1168e62658f9457ffb8fd4f18
pilz <leandro.pizz@gmail.com>
2019-06-26 23:36:37 -0300

    CAP.386 Autenticaçao Store

--- src/main.js
+++ src/main.js
@@ -9,9 +9,6 @@ import './config/msgs'
 
 Vue.config.productionTip = false
 
-//TEMPORARIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkxlYW5kcm8gUGlseiIsImVtYWlsIjoibGVhbmRyby5waWx6ekBnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTYxNTY3MzAxLCJleHAiOjE1NjE4MjY1MDF9.03YbpL_z5fN08hi33IYhqBKEQWcQxhSmWK_d9btEoHM'
-
 new Vue({
     store,
     router,
